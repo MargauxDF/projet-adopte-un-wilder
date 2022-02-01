caebb4613a1143aeef9a9af39a6bc0394e88de1a
Margaux De Filippis <margaux.defilippis@gmail.com>
2022-02-01 09:16:50 +0100

    modification education controller

--- src/Form/EducationType.php
+++ src/Form/EducationType.php
@@ -3,9 +3,9 @@
 namespace App\Form;
 
 use App\Entity\Education;
-use Doctrine\DBAL\Types\TextType;
 use phpDocumentor\Reflection\DocBlock\Description;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
