300648faac2e6dbbb5b55e5346d9394c175479ff
amaria-foukia <amaria.foukia@gmail.com>
2022-02-03 22:28:08 +0100

    modif InfoController et routes associées

--- src/Form/InfoType.php
+++ src/Form/InfoType.php
@@ -29,28 +29,11 @@ class InfoType extends AbstractType
             ->add('presentation')
             ->add('picture', FileType::class, [
                 'label' => 'Picture (Image file)',
-
                 // unmapped means that this field is not associated to any entity property
                 'mapped' => false,
-
                 // make it optional so you don't have to re-upload the PDF file
                 // every time you edit the Product details
-                'required' => false,
-
-                // unmapped fields can't define their validation using annotations
-                // in the associated entity, so you can use the PHP constraint classes
-                /*'constraints' => [
-                    new File([
-                        'maxSize' => '1024k',
-                        'mimeTypes' => [
-                            'application/jpg',
-                            'application/jpeg',
-                            'application/png',
-                        ],
-                        'mimeTypesMessage' => 'Please upload a valid picture',
-                    ])
-                ],*/
-            ])
+                'required' => false,])
             ->add('linkedin')
             ->add('github')
             ->add('twitter')
